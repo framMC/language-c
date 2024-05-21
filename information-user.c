45f03b01744f18034f915de22e656d49cf188faf
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-05-21 16:40:23 +0700

    Information user language c

--- information-user.c
+++ information-user.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
+ //กรุณาลยคำตอบใน (""); ทุกครั้งเเล้วเติมคำตอบที่ต้องการลงไป
     printf("ชื่อ-นาสกุล\n");
     printf("รหัสประจำตัว\n");
     printf("วันเดือนปีเกิด\n");
     printf("ชื่อโรงเรียน ชั้นปีที่\n");
-  //Code Written By FramMC
+ //Code Written By FramMC
     return 0;
 }
