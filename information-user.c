41688506b6de1f4086c972f64617764825942643
VernonSecV2 <108213355+framMC@users.noreply.github.com>
2024-05-21 16:38:36 +0700

    Create information-user.c

---
+++ information-user.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("ชื่อ-นาสกุล\n");
+    printf("รหัสประจำตัว\n");
+    printf("วันเดือนปีเกิด\n");
+    printf("ชื่อโรงเรียน ชั้นปีที่\n");
+  //Code Written By FramMC
+    return 0;
+}
